c1fbb46f699e1116806f2526c8178aee82859b97
Soroush <shuoros@yahoo.com>
2022-12-18 10:30:00 +0100

    Init Spring Boot files

---
+++ src/main/java/com/github/nazaninhmz/danceHall/DanceHallApplication.java
@@ -0,0 +1,13 @@
+package com.github.nazaninhmz.danceHall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanceHallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanceHallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nazaninhmz/danceHall/DanceHallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nazaninhmz.danceHall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanceHallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
